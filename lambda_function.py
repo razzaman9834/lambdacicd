5e3cd95a1b7fac3eadc9a32fd97a2132266b0d78
Aman Raj <razzaman9834@gmail.com>
2025-08-20 17:48:51 +0530

    nextcommit

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,9 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
+    a="aman"
     return {
+        'name':a,
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!....... (Modified v0.2')
     }
